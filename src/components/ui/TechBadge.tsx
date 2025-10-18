59239824afdc60e4b4348b4a549956ffc186f36a
antoniorusso2 <russoantonio303@outlook.it>
2025-10-18 18:34:00 +0200

    fix isColorLight helper cfor dynamic text color on badges based on bg color

--- src/components/ui/TechBadge.tsx
+++ src/components/ui/TechBadge.tsx
@@ -4,12 +4,12 @@ import type { TechBadgeProps } from "../../types/technology";
 
 export default function TechBadge({ technology }: TechBadgeProps): JSX.Element {
     const textColor = isColorLight(technology.color)
-        ? "text-zinc-800"
+        ? "text-(--color-text-dark)"
         : "text-zinc-200";
 
     return (
         <span
-            className={`badge text-sm fw-bold py-0.5 px-1 rounded-xs ${textColor}`}
+            className={`badge text-md font-bold py-0.5 px-1 rounded-xs ${textColor}`}
             style={{ backgroundColor: technology.color }}
             key={technology.id}
         >
--- src/data/technologies.ts
+++ src/data/technologies.ts
@@ -5,7 +5,7 @@ const technologies: Technology[] = [
         id: 1,
         name: "HTML",
         level: 4,
-        color: "#ff6d01",
+        color: "#E65100",
         icon_external_url:
             "https://img.icons8.com/?size=100&id=20909&format=png&color=000000",
     },
@@ -13,7 +13,7 @@ const technologies: Technology[] = [
         id: 2,
         name: "CSS",
         level: 1,
-        color: "#1377bd",
+        color: "#2062AF",
         icon_external_url:
             "https://img.icons8.com/?size=100&id=21278&format=png&color=000000",
     },
@@ -21,7 +21,7 @@ const technologies: Technology[] = [
         id: 3,
         name: "JavaScript",
         level: 1,
-        color: "#fdd600",
+        color: "#F7DF1E",
         icon_external_url:
             "https://img.icons8.com/?size=100&id=108784&format=png&color=000000",
     },
@@ -45,7 +45,7 @@ const technologies: Technology[] = [
         id: 6,
         name: "React",
         level: 1,
-        color: "#00acc1",
+        color: "#53C1DE",
         icon_external_url:
             "https://img.icons8.com/?size=100&id=asWSSTBrDlTW&format=png&color=000000",
     },
@@ -53,7 +53,7 @@ const technologies: Technology[] = [
         id: 7,
         name: "Node.js",
         level: 1,
-        color: "#4caf50",
+        color: "#21A366",
         icon_external_url:
             "https://img.icons8.com/?size=100&id=54087&format=png&color=000000",
     },
@@ -61,7 +61,7 @@ const technologies: Technology[] = [
         id: 8,
         name: "Bootstrap",
         level: 1,
-        color: "#7c4dff",
+        color: "#7C4DFF",
         icon_external_url:
             "https://img.icons8.com/?size=100&id=PndQWK6M1Hjo&format=png&color=000000",
     },
@@ -69,7 +69,7 @@ const technologies: Technology[] = [
         id: 9,
         name: "Tailwind",
         level: 4,
-        color: "#00acc1",
+        color: "#00ACC1",
         icon_external_url:
             "https://img.icons8.com/?size=100&id=4PiNHtUJVbLs&format=png&color=000000",
     },
@@ -85,7 +85,7 @@ const technologies: Technology[] = [
         id: 11,
         name: "MySQL",
         level: 1,
-        color: "#4caf50",
+        color: "#00796B",
         icon_external_url:
             "https://img.icons8.com/?size=100&id=9nLaR5KFGjN0&format=png&color=000000/possimus-quod-dolores-sed-et-nam-inventore.html",
     },
@@ -93,9 +93,9 @@ const technologies: Technology[] = [
         id: 12,
         name: "Java",
         level: 3,
-        color: "#ffff00",
+        color: "#1565C0",
         icon_external_url:
-            "https://img.icons8.com/?size=100&id=FRRACRKRsw2s&format=png&color=000000",
+            "https://img.icons8.com/?size=100&id=13679&format=png&color=000000",
     },
 ];
 
